5d3dd67b7e4e2b161952c3e3f9299163153aadbc
yanjunzheng <yanjunzheng@pset.suntec.net>
2018-06-04 09:54:02 +0800

    commit test
    Change-Id: I233bddac054ea073a0fac6bda5ff9237f6abec30

--- socket/tcp/client.py
+++ socket/tcp/client.py
@@ -4,7 +4,7 @@ import socket
 import time
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.connect(('localhost', 8001))
+sock.connect(('116.62.18.190', 8001))
 sock.send('1')
 print sock.recv(1024)
 sock.close()
